63c948a224783419e5052e19ac09d8528aa9c137
Matthew Pickering <matthewtpickering@gmail.com>
2018-09-12 12:20:59 +0000

    Pin nixpkgs

--- site.nix
+++ site.nix
@@ -1,5 +1,14 @@
 let
-  nixpkgs = import <nixpkgs> {};
+  _nixpkgs = import <nixpkgs> {};
+
+  nixpkgsSrc = _nixpkgs.fetchFromGitHub {
+    owner  = "NixOS";
+    repo   = "nixpkgs-channels";
+    rev    = "2f6440eb09b7e6e3322720ac91ce7e2cdeb413f9";
+    sha256 = "0vb7ikjscrp2rw0dfw6pilxqpjm50l5qg2x2mn1vfh93dkl2aan7";
+  };
+
+  nixpkgs = import nixpkgsSrc {};
 
   ghc = nixpkgs.haskell.packages.ghc822.ghcWithPackages (ps: with ps;
         [ ps.hakyll
